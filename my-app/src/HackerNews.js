a6a14e1c38da797bf1e78f9cc11432213a5010d6
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-10-28 14:40:35 -0400

    update

--- my-app/src/HackerNews.js
+++ my-app/src/HackerNews.js
@@ -76,21 +76,18 @@ class HackerNews extends Component {
         <div className="cardContainer">
 
           <Card
+            zDepth={1}
             style={{
               backgroundColor: '#333',
-              height: '34px',
+              height: '50px',
               width: '950px',
               iconElementLeft: 'none',
             }}
           >
-            <CardHeader
-              title="Hacker News"
-              titleColor={grey50}
-              style={{
-                paddingLeft: '10px',
-              }}
-            />
-
+            <div  className="nav-bar">
+              <h4 className="logo" titleColor={grey50} style={{ paddingLeft: '10px', fontSize: '15px', color: '#fff', fontFamily: 'Roboto, sans-serif'}}>HACKER NEWS</h4>
+              <FlatButton hoverColor='#333' className="title-link" label='new' href={'/'} style={{ marginTop: '11px', fontFamily: 'Roboto, sans-serif', color: '#f2f2f2', fontSize: '15px'}}/>
+            </div>
           </Card>
 
           <div className="storyCard">
@@ -106,7 +103,8 @@ class HackerNews extends Component {
                       paddingLeft: '20px',
                       textAlign: 'left',
                       color: '#333',
-                      alignContent: 'center'
+                      alignContent: 'center',
+                      backgroundColor: '#f2f2f2'
                     }}
                   >
                 <CardText>
@@ -114,7 +112,7 @@ class HackerNews extends Component {
                 <h4>{storydata.number}.</h4><FlatButton className="title-link" label={storydata.title} hoverColor={grey50} href={storydata.url} style={{ marginTop: '9px', fontFamily: 'Roboto, sans-serif'}}/>
                 </div>
 
-                <h4>Score: {storydata.score} | Author's Id:{storydata.by} | Author's Karma Score:{storydata.karma}  | Timestamp:{storydata.time}</h4>
+                <h4>Score: {storydata.score} | Author's Id: {storydata.by} | Author's Karma Score: {storydata.karma}  | Timestamp: {storydata.time}</h4>
                 </CardText>
                   </Card>
                 </div>
