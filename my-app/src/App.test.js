5a6a3c585adf3ac69e0ba865179e6fdfbfb89fdb
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-10-29 17:09:34 -0400

    tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { mount } from 'enzyme';
-import { expect } from 'chai';
-import React from 'react';
-import HackerNews from './HackerNews';
+// require('sinon-as-promised');
+// import { expect } from 'chai';
+// import { mount } from 'enzyme';
+// import sinon from 'sinon';
+// import React from 'react';
+// import HackerNews from './HackerNews';
 
+// describe('HackerNews', () => {
+//   it('calls componentDidMount',() => {
 
-describe('HackerNews', () => {
-  it('calls componentDidMount',() => {
+//     const componentDidMount = sinon.stub().resolves();
+//     const component = mount(<HackerNews componentDidMount={componentDidMount} />);
+//     expect(componentDidMount.callCount).to.equal(1);
+//   });
 
-    const componentDidMount = sinon.stub();
-    const component = mount(<HackerNews componentDidMount={componentDidMount} />);
-    expect(componentDidMount.callCount).to.equal(1);
-
-    // const storiesData = [
-    //   {id: 1, title: 'TestOne', body:'Foo One'},
-    //   {id: 2, title: 'TestTwo', body: 'Foo Two'},
-    //   {id: 3, title: 'TestThree', body: 'Foo Three'}
-    // ];
-
-    // expect(component.state('loaded')).is.equal(true);
-    // expect(component.state('topstories')).is.equal.to(someData);
-  });
-  it('sets component state to returned data')
-  it('calls the topstories endpoint')
-  it('calls the item endpoint')
-  it('calls the user endpoint')
-})
+//   it('sets component state to returned data')
+//   it('calls the topstories endpoint')
+//   it('calls the item endpoint')
+//   it('calls the user endpoint')
+// });
